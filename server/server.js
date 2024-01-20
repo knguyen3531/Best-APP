c9ed1018df0a79890f16d816b0b814f2fcf7d351
khoi <knguyen3531@gmail.com>
2024-01-20 19:05:07 +1100

    Update server port configuration

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+// Adjust the path to serve static files from the 'client/dist' directory
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Include your routes
 require('./routes/htmlRoutes')(app);
 
+// Start the server
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
